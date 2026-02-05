162199b65fa51ac341d365670190c83f292eed5c
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-02-05 17:21:24 +0530

    area of traingle

---
+++ p6.py
@@ -0,0 +1,6 @@
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+c = float(input("Enter side c: "))
+s = (a + b + c) / 2
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print("Area of triangle:", area)
