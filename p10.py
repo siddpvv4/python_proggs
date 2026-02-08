317775c98e8a44510ff4af8a0e3fc693e796eac5
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-02-08 13:49:06 +0530

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,13 @@
+print("A B A&B A|B A^B")
+a = 0
+b = 0
+print(a, b, a & b, a | b, ~a&b|~b&a)
+a = 0
+b = 1
+print(a, b, a & b, a | b,~a&b|~b&a)
+a = 1
+b = 0
+print(a, b, a & b, a | b, ~a&b|~b&a)
+a = 1
+b = 1
+print(a, b, a & b, a | b,~a&b|~b&a)
