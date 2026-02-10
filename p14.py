2f52e7b03ec7597ac2e270c3690b713c5663323a
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-02-10 16:35:48 +0530

    duvisible by 5

---
+++ p14.py
@@ -0,0 +1,6 @@
+#2. Check whether a number is multiple of 5 or not
+num = int(input("Enter a number: "))
+if num % 5 == 0:
+    print("Number is a multiple of 5")
+else:
+    print("Number is NOT a multiple of 5")
