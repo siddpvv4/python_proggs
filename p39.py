9f00e3ea007296383b374a52c50fc1ea16e168d8
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-02-24 10:11:20 +0530

    Set operations

---
+++ p39.py
@@ -0,0 +1,9 @@
+#Set operations
+S1 = {"Red", "yellow", "orange", "blue","purple"}
+S2 = {"violet", "blue", "purple"}
+
+print("Union:", S1 | S2)
+print("Intersection:", S1 & S2)
+print("S1 - S2:", S1 - S2)
+print("S2 - S1:", S2 - S1)
+print("Symmetric Difference:", S1 ^ S2)
