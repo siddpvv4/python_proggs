c7cb4bea8cb392eab300cce87693585d0dbed26d
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-04-15 09:57:46 +0530

    Sum of all elements in NumPy array

---
+++ p71.py
@@ -0,0 +1,10 @@
+#Sum of all elements in NumPy array
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+total_sum = np.sum(arr)
+
+print("Array:", arr)
+print("Sum of all elements:", total_sum)
